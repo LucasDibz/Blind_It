68ccd15a0d1d6d8d819e006ad0c89709ac82a353
= <lucas.f.dib@gmail.com>
2020-07-11 17:24:25 -0300

    Added Update/Delete User routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -6,8 +6,13 @@ const usersController = new UsersController();
 
 const routes = express.Router();
 
+//Login
 routes.post('/user', usersController.login);
+
+//Users
 routes.get('/users', usersController.index);
 routes.post('/users', usersController.create);
+routes.patch('/users/:id', usersController.update);
+routes.delete('/users/:id', usersController.delete);
 
 export default routes;
